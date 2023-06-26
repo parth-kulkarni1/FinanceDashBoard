6c67db8267d0883005c28fcc66b7a0c73dfc9ef6
parth-kulkarni-1 <parth.kulkarni2705@gmail.com>
2023-06-26 22:01:14 +1000

    Added Implementation of Redis Store

--- express/dist/index.js
+++ express/dist/index.js
@@ -5,9 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const Routes_1 = require("./Routes/Routes");
+const sessionExpiration_1 = __importDefault(require("./sessionExpiration"));
 const express_session_1 = __importDefault(require("express-session"));
 const helmet_1 = __importDefault(require("helmet"));
-const sessionExpiration_1 = __importDefault(require("./sessionExpiration"));
+const redis_1 = require("redis");
+const connect_redis_1 = __importDefault(require("connect-redis"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 4000;
 const cors = require('cors');
@@ -15,25 +17,34 @@ const cors_options = {
     origin: ["https://up-bank-dashboard.netlify.app", "https://up-bank-dashboard.netlify.app/", "http://localhost:3000/"],
     credentials: true
 };
+const redisURL = process.env.REDIS_URL;
+const redisPassword = process.env.REDISPASSWORD;
+let redisClient = (0, redis_1.createClient)({ url: redisURL });
+redisClient.connect().catch(console.error);
+let redisStore = new connect_redis_1.default({
+    client: redisClient,
+    prefix: "UP-APP-COOKIE",
+});
 app.use(express_1.default.json());
 app.use(cors(cors_options));
 app.use((0, helmet_1.default)());
 // Set trust proxy if you are behind a reverse proxy like railways.app
 // This allows secure cookies to work properly
 app.set('trust proxy', 1);
-app.use((0, express_session_1.default)({
+const sessionOptions = {
+    store: redisStore,
     secret: "keyboard cat",
     saveUninitialized: true,
     resave: false,
-    name: "UP-APP-COOKIE",
     cookie: {
         httpOnly: true,
         maxAge: 1800000,
         secure: true,
         sameSite: 'none',
     },
-}));
+};
+app.use((0, express_session_1.default)(sessionOptions));
 app.use(sessionExpiration_1.default);
 app.use(Routes_1.router);
-app.listen(port, () => console.log("sucessfully created on port 4000"));
+app.listen(port, () => console.log("successfully created on port 4000"));
 module.exports = app;
--- express/index.ts
+++ express/index.ts
@@ -1,9 +1,11 @@
 import express, { Router } from "express";
 import { router } from "./Routes/Routes";
-import session from "express-session";
-import helmet from 'helmet'
 import checkSessionExpiration from "./sessionExpiration";
 
+import session, { SessionOptions } from "express-session";
+import helmet from 'helmet'
+import {createClient} from 'redis';
+import RedisStore from "connect-redis"
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -14,35 +16,45 @@ const cors_options = {
 }
 
 
+const redisURL = process.env.REDIS_URL;
+const redisPassword = process.env.REDISPASSWORD;
+
+
+let redisClient = createClient({url: redisURL});
+redisClient.connect().catch(console.error)
+
+let redisStore = new RedisStore({
+  client: redisClient,
+  prefix: "UP-APP-COOKIE",
+})
+
 app.use(express.json());
-app.use(cors(cors_options)) 
-app.use(helmet())
+app.use(cors(cors_options));
+app.use(helmet());
 
 // Set trust proxy if you are behind a reverse proxy like railways.app
 // This allows secure cookies to work properly
 app.set('trust proxy', 1);
 
-
-app.use(
-  session({
+const sessionOptions: SessionOptions = {
+    store: redisStore,
     secret: "keyboard cat",
     saveUninitialized: true,
     resave: false,
-    name: "UP-APP-COOKIE",
     cookie: {
       httpOnly: true,
       maxAge: 1800000,
       secure: true, // Set 'secure' to true to ensure the cookie is sent only over HTTPS
       sameSite: 'none',
     },
-  })
-);
+}
 
-app.use(checkSessionExpiration)
+app.use(session(sessionOptions));
 
-app.use(router)
+app.use(checkSessionExpiration);
 
-app.listen(port, () => console.log("sucessfully created on port 4000"));
+app.use(router);
 
-module.exports = app
+app.listen(port, () => console.log("successfully created on port 4000"));
 
+module.exports = app;
